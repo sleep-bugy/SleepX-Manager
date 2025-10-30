db04d7ecb450f4e29663e87fd700d59e77d878f9
sleep-bugy <sleep-bugy@users.noreply.github.com>
2025-10-30 17:17:35 +0700

    Build: add JitPack repository for libsu resolution

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,6 +15,7 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://jitpack.io") }
     }
 }
 rootProject.name = "KernelManagerUI"
