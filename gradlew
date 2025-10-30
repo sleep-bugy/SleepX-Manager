1dffe646520b29ee8c61f78669f7728cf0793851
sleep-bugy <sleep-bugy@users.noreply.github.com>
2025-10-30 16:50:20 +0700

    Fix Gradle wrapper scripts and properties for Gradle 8.9

--- gradlew
+++ gradlew
@@ -1,2 +1,12 @@
 #!/usr/bin/env sh
-./gradlew
+
+APP_HOME="$(cd "$(dirname "$0")"; pwd -P)"
+CLASSPATH="$APP_HOME/gradle/wrapper/gradle-wrapper-main.jar:$APP_HOME/gradle/wrapper/gradle-wrapper-shared.jar"
+
+if [ -n "$JAVA_HOME" ] ; then
+  JAVA_EXE="$JAVA_HOME/bin/java"
+else
+  JAVA_EXE="java"
+fi
+
+exec "$JAVA_EXE" -Dorg.gradle.appname=gradlew -classpath "$CLASSPATH" org.gradle.wrapper.GradleWrapperMain "$@"
--- gradlew.bat
+++ gradlew.bat
@@ -1,2 +1,14 @@
 @echo off
-call gradlew.bat
+setlocal
+
+set DIR=%~dp0
+set APP_HOME=%DIR%
+for %%i in ("%APP_HOME%..") do set APP_HOME=%%~fi
+
+set CLASSPATH="%APP_HOME%\gradle\wrapper\gradle-wrapper-main.jar;%APP_HOME%\gradle\wrapper\gradle-wrapper-shared.jar"
+
+set JAVA_EXE=java
+if defined JAVA_HOME set JAVA_EXE="%JAVA_HOME%\bin\java.exe"
+
+%JAVA_EXE% -Dorg.gradle.appname=gradlew -classpath %CLASSPATH% org.gradle.wrapper.GradleWrapperMain %*
+exit /b %ERRORLEVEL%
