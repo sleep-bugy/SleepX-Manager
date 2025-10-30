5649351590aa0e237a4614e7128a340d770a3db0
sleep-bugy <sleep-bugy@users.noreply.github.com>
2025-10-30 17:04:11 +0700

    Build: define plugin versions in settings (AGP 8.7.2, Kotlin 2.0.20, Compose plugin)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,6 +4,11 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
+    plugins {
+        id("com.android.application") version "8.7.2"
+        id("org.jetbrains.kotlin.android") version "2.0.20"
+        id("org.jetbrains.kotlin.plugin.compose") version "2.0.20"
+    }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
